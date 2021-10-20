44f97e64ee5abb3fb716216b1673f156937a3240
John <55846467+johncain2000@users.noreply.github.com>
2021-10-20 14:44:32 -0400

    Update sensor_example.c

--- sensor_example.c
+++ sensor_example.c
@@ -2,12 +2,20 @@ const int AirValue = 620;   //you need to replace this value with Value_1
 const int WaterValue = 310;  //you need to replace this value with Value_2
 int soilMoistureValue = 0;
 int soilmoisturepercent=0;
+
+//blink leds based on soil moisture reading
+int led1 = 13;
+int led2 = 12;
+
 void setup() {
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 void loop() {
+//digitalWrite(led1, HIGH);
 soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
-Serial.println(soilMoistureValue);
+//Serial.println(soilMoistureValue);
 soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
 if(soilmoisturepercent >= 100)
 {
@@ -21,7 +29,25 @@ else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
 {
   Serial.print(soilmoisturepercent);
   Serial.println("%");
+  if(soilmoisturepercent >= 50){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+  } else {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+  }
   
 }
   delay(250);
 }
+
+//void loop() {
+//  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(100);               // wait for a second
+//  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+//  delay(100); 
+//  digitalWrite(led2, HIGH);
+//  delay(100);
+//  digitalWrite(led2, LOW);
+//  delay(100);
+// }
