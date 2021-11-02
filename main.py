f2b5c428dcdf519cbc6890adf04aa3641683afb4
John Cain <john.cain@student.fairfield.edu>
2021-11-02 01:01:31 -0400

    fingerprint id generation

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # python can interact with a PLC via ethernet port
+# import pycomm3
 
-# test push
+# from pycomm3 import LogixDriver
+
+# with LogixDriver('IP_ADDRESS_HERE') as 
+
+import secrets
+
+def generateFingerprintId():
+	bits = secrets.randbits(256)
+	bits_hex = hex(bits)
+	fingerprintID = bits_hex[2:]
+
+	return fingerprintID
+
+print(generateFingerprintId())
